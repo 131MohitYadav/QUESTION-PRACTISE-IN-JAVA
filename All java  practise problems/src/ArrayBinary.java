7c844c24e07c54ab74e5b42adce6c1f09bd69892
131MohitYadav <yadavjimohit54@gmail.com>
2024-11-28 16:28:51 +0530

    Array programe

---
+++ All java  practise problems/src/ArrayBinary.java
@@ -0,0 +1,34 @@
+
+// write a java programme to convert given number in binary form with the help of array
+
+import java.util.Scanner;
+public class ArrayBinary {
+
+    public static void main(String args [ ]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER ANY NUMBER: ");
+        int n = sc.nextInt();
+
+        int i  = 0;
+        int arr[] = new int[10];
+
+        while ( n > 0){//10
+            // for binary number or you can also find the hexadecimal 16 and octal 8
+            int r = n % 2;//5
+            arr[i] = r;// 5 in 0 index
+            i++;
+            n = n/2;
+        }
+
+  System.out.println("Binary Number is:");
+        // ye hmm esleye user kr rahi taki digit print lsb to msb
+        for (  i = i-1; i >= 0; i--){
+            System.out.print("\t"+arr[i]);
+        }
+
+
+
+    }
+}
---
+++ All java  practise problems/src/ArraySort.java
@@ -0,0 +1,28 @@
+
+// Write a java programe to check given array is sorted or not
+
+import java.util.Scanner;
+public class ArraySort {
+public static void main(String args [ ]){
+    Scanner sc = new Scanner(System.in);
+
+    int arr[] = {60,20,30,40,50};
+    int count = 0;
+
+
+    for(int i =0 ; i< arr.length; i++){
+        for( int j = i + 1; j < arr.length; j++){
+            if ( arr[i] > arr[j]){
+                count++;
+            }
+        }
+    }
+    if ( count == 0){
+        System.out.println("Yes Array is sorted");
+    }
+    else{
+        System.out.println("No Array is not sorted");
+    }
+
+    }
+}
