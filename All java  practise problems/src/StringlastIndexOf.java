d580989f3d53e264139c8905d66d95c8a7986a21
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-07 00:46:58 +0530

    7 March 2025

--- All java  practise problems/src/StringlastIndexOf.java
+++ All java  practise problems/src/StringlastIndexOf.java
@@ -2,7 +2,9 @@ public class StringlastIndexOf {
 
     public static  void main(String args[]){
         String s1 = "Hello Mohit yadav";
+        String s2 = "Welcome to bhopal";
         // lastindexof -> it give integer value
         System.out.println(s1.lastIndexOf("av"));
+        System.out.println(s2.lastIndexOf("wel"));
     }
 }
