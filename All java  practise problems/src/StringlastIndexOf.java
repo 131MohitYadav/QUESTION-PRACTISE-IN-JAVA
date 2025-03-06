5796faebac6bdd0059dd79b3b40e6bdabf1c5595
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-07 00:14:16 +0530

    7 March 2025

---
+++ All java  practise problems/src/StringlastIndexOf.java
@@ -0,0 +1,8 @@
+public class StringlastIndexOf {
+
+    public static  void main(String args[]){
+        String s1 = "Hello Mohit yadav";
+        // lastindexof -> it give integer value
+        System.out.println(s1.lastIndexOf("av"));
+    }
+}
