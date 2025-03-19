7e43e046a9585dca343f754cb5807a3b8e79f445
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-19 21:25:25 +0530

    19 March 2025

--- All java  practise problems/src/Stringsplit.java
+++ All java  practise problems/src/Stringsplit.java
@@ -5,7 +5,10 @@ public class Stringsplit {
         String s1 = "Welcome to Bhopal Mohit Yadav";
         String arr [] = s1.split("o");
        // System.out.println(arr[5]); // it give arrayindexoutofbound exception
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
+        System.out.println(arr[0]);// wel
+        System.out.println(arr[1]);// me t
+        System.out.println(arr[2]); // bh
+        System.out.println(arr[3]); // pal M
+        System.out.println(arr[4]);// hit Yadav
     }
 }
