06b01cf491d6da564524c250b4bbd362f408678d
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-20 12:24:18 +0530

    20 March 2025

---
+++ All java  practise problems/src/Stringsubstring.java
@@ -0,0 +1,11 @@
+public class Stringsubstring {
+    public static  void main(String args[]){
+        String s1 = "Hello Mohit";
+        String s2 = "Welcome back to Bhopal";
+        System.out.println(s1.substring(5)); // Mohit
+        System.out.println(s1.substring(3,6)); // lo
+        String x = s2.substring(4,8);// ome
+        System.out.println(x);
+
+    }
+}
