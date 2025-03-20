e468205db02676435484cd93303b806db82a2fa9
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-20 12:35:10 +0530

    20 March 2025

--- All java  practise problems/src/StringForeach.java
+++ All java  practise problems/src/StringForeach.java
@@ -2,18 +2,28 @@ import java.util.*;
 
 public class StringForeach {
     public  static void  main(String [] args){
-        int arr[] = {10,20,30,40};
-        String s[] = {"hello"};
-        char [] c = {'M','O','H','I','T'};
-        for ( int a : arr){
-            System.out.println(a);
+//        int arr[] = {10,20,30,40};
+//        String s[] = {"hello"};
+//        char [] c = {'M','O','H','I','T'};
+//        for ( int a : arr){
+//            System.out.println(a);
+//
+//        }
+//        for ( String b : s){
+//            System.out.println(b);
+//        }
+//        for ( char d : c){
+//            System.out.print(d);
+//        }
+
+        int arr [] = {10,30,40,50,13};
+
+        int sum = 0;
+        for ( int x : arr){
+            sum = sum + x;
 
         }
-        for ( String b : s){
-            System.out.println(b);
-        }
-        for ( char d : c){
-            System.out.print(d);
-        }
+        System.out.println("Sum of : " + sum);
+        System.out.println("Average of : " + arr.length/2);
     }
 }
