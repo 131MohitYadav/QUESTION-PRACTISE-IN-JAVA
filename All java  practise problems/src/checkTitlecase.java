d7e9c9d2a587f545bad7cb306f071b3b80bd27a0
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-21 12:46:13 +0530

    21 March 2025

---
+++ All java  practise problems/src/checkTitlecase.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class checkTitlecase {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any String : " );
+        String s = sc.nextLine();
+
+        String r = "";
+        for ( int x : s.toCharArray()){
+            if ( x >= 'a' && x <= 'z'){
+                x = x - 32;
+            }
+            else if( x >= 'A' && x <= 'Z'){
+                x = x + 32;
+            }
+                r = r + (char) x;
+            }
+
+
+        System.out.println(r);
+    }
+}
