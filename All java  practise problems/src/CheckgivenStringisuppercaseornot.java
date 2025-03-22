56f35d5c7a0e92f41109927a629ca5ff044ff27d
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-22 12:24:47 +0530

    22 March 2025

---
+++ All java  practise problems/src/CheckgivenStringisuppercaseornot.java
@@ -0,0 +1,15 @@
+
+import java.sql.SQLOutput;
+import java.util.*;
+public class CheckgivenStringisuppercaseornot {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ENTER ANY STRING: ");
+        String s = sc.nextLine();
+
+        System.out.println(s.toUpperCase().equals(s));
+    }
+
+}
