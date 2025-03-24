b8e5588e2fd72bc40cc834db6f14dea1179dfd80
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-24 11:17:08 +0530

    24 March 2025

--- All java  practise problems/src/CheckTitleCase.java
+++ All java  practise problems/src/CheckTitleCase.java
@@ -6,8 +6,20 @@ public class CheckTitleCase {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("ENTER ANY STRING FOR CHECK TITLE CASE OR NOT:  ");
+        System.out.print("ENTER ANY STRING FOR CHECK TITLE CASE OR NOT:  ");
         String s = sc.nextLine();
+
+        boolean Titlecase = true;
+        String w[] = s.split(" ");
+        for ( String word : w){
+
+            if ( !Character.isUpperCase(word.charAt(0)) || !word.substring(1).equals(word.substring(1).toLowerCase())){
+                Titlecase = false;
+                break;
+            }
+
+        }
+        System.out.println(Titlecase);
 
 
     }
