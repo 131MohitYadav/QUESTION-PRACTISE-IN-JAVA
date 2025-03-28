f0b36fa988c0217c26bf6d005beffadb7979a67c
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-28 22:10:04 +0530

    28 march 2025

---
+++ All java  practise problems/src/StringtoString.java
@@ -0,0 +1,9 @@
+public class StringtoString {
+
+    public static void main(String args[]){
+
+        Integer y = 1234343;
+        String r = y.toString();
+        System.out.println(r.length());
+    }
+}
