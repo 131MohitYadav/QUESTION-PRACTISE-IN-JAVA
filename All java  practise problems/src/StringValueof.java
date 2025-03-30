890b889c3bd77c52afde0d79f385b9aa64f5ec2c
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-30 21:35:07 +0530

    30 march 2025

--- All java  practise problems/src/StringValueof.java
+++ All java  practise problems/src/StringValueof.java
@@ -4,5 +4,18 @@ public class StringValueof {
         Integer x = 33345;
         String a = String.valueOf(x);
         System.out.println(a.length()); // 5
+
+        float  y = 2343.234f;
+        String b = String.valueOf(y);
+        System.out.println(b.length()); // 8
+
+        char z = 'M';
+        String c = String.valueOf(z);
+        System.out.println(c.length());
+
+        String s = "Hello Mohit";
+        String w = String.valueOf(s);
+        System.out.println(w.length()); // 11
+
     }
 }
