45f0676607799101138dfec839f13731e8dc8313
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-30 21:39:42 +0530

    30 march 2025

---
+++ All java  practise problems/src/Stringtrim.java
@@ -0,0 +1,8 @@
+public class Stringtrim {
+    public static void main(String args[]){
+
+        System.out.println("Length of the String before the trim: ");
+        String s1 = "Hello World ";
+
+    }
+}
