88f4af2511545b46a3544a3047bf3b0b9c477026
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-30 21:30:23 +0530

    30 march 2025

---
+++ All java  practise problems/src/StringValueof.java
@@ -0,0 +1,8 @@
+public class StringValueof {
+    public static void main(String args[]){
+
+        Integer x = 33345;
+        String a = String.valueOf(x);
+        System.out.println(a.length()); // 5
+    }
+}
