ddbad4184b28a0514c535bff56d63874d69fa44b
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-31 12:00:32 +0530

    31 march 2025

---
+++ All java  practise problems/src/Stringjoin.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Stringjoin {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any String; ");
+        String s = sc.nextLine();
+
+        String arr[] = s.split(" ");
+        String r = String.join("|" , arr[0] , arr[1] , arr[2]);
+        System.out.println(r);
+    }
+
+
+}
--- All java  practise problems/src/Stringtrim.java
+++ All java  practise problems/src/Stringtrim.java
@@ -1,8 +1,11 @@
 public class Stringtrim {
     public static void main(String args[]){
 
-        System.out.println("Length of the String before the trim: ");
-        String s1 = "Hello World ";
+        //System.out.println("Length of the String before the trim: ");
+        String s1 = " Hello World ";
+        System.out.println("length of the String before the trim: " + s1.length());
+        String s = s1.trim();
+        System.out.println("Length of the String After the trim: " + s.length());
 
     }
 }
