e7baeae5dc2665f005e15545346e67d87903d86f
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-31 21:22:27 +0530

    31 march 2025

--- All java  practise problems/src/Stringjoin.java
+++ All java  practise problems/src/Stringjoin.java
@@ -6,13 +6,19 @@ public class Stringjoin {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter any String; ");
+        System.out.println("Enter any String1; ");
         String s = sc.nextLine();
 
+        System.out.println("Enter String2 : ");
+        String a = sc.nextLine();
+
         String arr[] = s.split(" ");
+        String arr1[] = a.split(" ");
         String r = String.join(":" , arr);
+        String k = String.join("|" , arr1);
 //        String r = String.join("|" , arr[0] , arr[1] , arr[2]);
         System.out.println(r);
+        System.out.println(k);
     }
 
 
