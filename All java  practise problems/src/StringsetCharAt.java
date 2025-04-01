df4fe292d854ad2bb27add2f72e6d20ba6d1d75a
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-01 08:41:03 +0530

    1 april 2025

---
+++ All java  practise problems/src/StringsetCharAt.java
@@ -0,0 +1,8 @@
+public class StringsetCharAt {
+    public static void  main(String args[]){
+
+        StringBuffer s = new StringBuffer("AMAN");
+        s.setCharAt(0,'N');
+        System.out.println(s);
+    }
+}
