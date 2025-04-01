cf252de80dbb5e04fd4e60561c9a29708fb52d02
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-01 09:35:52 +0530

    1 april 2025

---
+++ All java  practise problems/src/StringBufferreverse.java
@@ -0,0 +1,11 @@
+public class StringBufferreverse {
+
+    public static void main(String args[]){
+
+        StringBuffer s = new StringBuffer("Mohit");
+
+        System.out.println("Before the Reverese: " + s);
+        s.reverse();
+        System.out.println(" After the Reverse: " + s);
+    }
+}
