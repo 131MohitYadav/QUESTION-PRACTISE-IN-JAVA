6cff4923c6969fc22ac407dd82eba69689ba3b18
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-03 22:00:05 +0530

    3 march 2025

--- All java  practise problems/src/BasicRevision.java
+++ All java  practise problems/src/BasicRevision.java
@@ -4,6 +4,7 @@ public class BasicRevision {
     public static void main(String args[]){
 
         System.out.println("Hello world");
+        System.out.printf("Hello Rohit");
         System.out.print("Hello Mohit");
     }
 }
