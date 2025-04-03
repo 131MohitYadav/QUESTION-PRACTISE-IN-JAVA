c999224800610527926cc504a117f2d1098b1076
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-03 22:08:04 +0530

    3 march 2025

--- All java  practise problems/src/BasicRevision.java
+++ All java  practise problems/src/BasicRevision.java
@@ -6,10 +6,16 @@ public class BasicRevision {
 
 
         int n1 , n2 , n3;
+        float r , ar , cr;
 
         n1 = 23;
         n2 = 4;
 
+        r = 2.0f;
+
+        // perform the operation
+        ar =  3.14f * r * r;
+        cr = 2 * 3.14f * r;
         n3 = n1 + n2;
 
         System.out.println(n1);
@@ -21,5 +27,8 @@ public class BasicRevision {
         System.out.println("Hello world");
         System.out.printf("Hello Rohit");
         System.out.print("Hello Mohit");
+        System.out.println("Radius: "  + r);
+        System.out.println(" Area of cricel: " + ar);
+        System.out.println(" Circumferece of cirl; " + cr);
     }
 }
