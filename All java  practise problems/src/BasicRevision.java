bac228f54c0d77b2e8fad407c39457aed5869c35
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-03 22:03:25 +0530

    3 march 2025

--- All java  practise problems/src/BasicRevision.java
+++ All java  practise problems/src/BasicRevision.java
@@ -3,6 +3,18 @@ public class BasicRevision {
 
     public static void main(String args[]){
 
+
+
+        int n1 , n2 , n3;
+
+        n1 = 23;
+        n2 = 4;
+
+        n3 = n1 + n2;
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
         System.out.println("Hello world");
         System.out.printf("Hello Rohit");
         System.out.print("Hello Mohit");
