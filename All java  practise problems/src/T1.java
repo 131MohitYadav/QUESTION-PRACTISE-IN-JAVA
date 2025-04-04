b6025843252ba0905fc8ff515bfcdf29a57f3d9a
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-04 10:57:01 +0530

    4 april 2025

--- All java  practise problems/src/T1.java
+++ All java  practise problems/src/T1.java
@@ -1,42 +1,43 @@
 //
-//
-//// EXAMPLE 1 -> MULTITHREADNING USING EXTENDS THREAD
-//public class T1  extends  Thread{
-//
-//    public void run(){
-//        for ( int i = 1; i<=5; i++){
-//            System.out.println("Child Thread: " + i);
-//        }
-//    }
-//
-//
-//    public static void main(String args[]){
-//        T1 t = new T1();
-//        t.start();
-//        for ( int i = 1; i <= 5; i++){
-//            System.out.println("Main Thread: ");
-//        }
-//    }
-//}
 
+// EXAMPLE 1 -> MULTITHREADNING USING EXTENDS THREAD
+public class T1  implements  Runnable{
 
-
- // EXAMPLE 2 -> Multithreadning using implements Runnable interface it is a best as compare to extends thread
-
-public class T1 implements Runnable{
-    public  void run(){
-        for ( int i = 1; i <= 5; i++){
+    public void run(){
+        for ( int i = 1; i<=5; i++){
             System.out.println("Child Thread: " + i);
         }
     }
 
-    public static  void main(String args[]){
-        Thread t2 = new Thread(new T1()){
-            t2.start();
-            for(int i = 1; i<=5; i++){
-                System.out.println("Main Thread: " + i);
-            }
 
-
+    public static void main(String args[]){
+//        T1 t = new T1();
+        Thread t = new Thread(new T1());
+        t.start();
+        for ( int i = 1; i <= 5; i++){
+            System.out.println("Main Thread: ");
+        }
     }
 }
+
+//
+//
+// // EXAMPLE 2 -> Multithreadning using implements Runnable interface it is a best as compare to extends thread
+//
+//public class T1 implements Runnable{
+//    public  void run(){
+//        for ( int i = 1; i <= 5; i++){
+//            System.out.println("Child Thread: " + i);
+//        }
+//    }
+//
+//    public static  void main(String args[]){
+//        Thread t2 = new Thread(new T1()){
+//            t2.start();
+//            for(int i = 1; i<=5; i++){
+//                System.out.println("Main Thread: " + i);
+//            }
+//
+//
+//    }
+//}
