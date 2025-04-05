b9a25e38e6d8ac5d8bb63022f09386b5b9e9b466
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-05 12:06:49 +0530

    5 april 2025

---
+++ All java  practise problems/src/Swap.java
@@ -0,0 +1,20 @@
+
+// swap two number with uisng third variable
+public class Swap {
+
+    public static void main(String args[]){
+
+        int a , b , c;
+
+        a = 23;
+        b= 24;
+        c = 27;
+
+        c = a;
+        b = c;
+        c = b;
+        System.out.println("A number; " + a);
+        System.out.println("B number : " + b);
+        System.out.println("C number: " + c);
+    }
+}
