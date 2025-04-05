e81c119ef277369c103fb79f6663716f65275e21
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-05 12:15:59 +0530

    5 april 2025

---
+++ All java  practise problems/src/SwapWithout.java
@@ -0,0 +1,16 @@
+public class SwapWithout {
+
+    public static void main(String args[]) {
+        int a, b;
+        a = 10;
+        b = 3;
+
+        System.out.println("Befor Swapping " + a);
+        System.out.println(" Befor swaping :  " + b);
+
+        a = a + b; // 10 + 3  13
+        b = a - b; // 10 - 3 = 7
+        a = a - b; // 10 - 3 =
+    }
+}
+
