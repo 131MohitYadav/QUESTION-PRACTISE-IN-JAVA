3bea2563e5f6d77a7419bc224de80371a86c6d51
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-06 18:38:09 +0530

    6 april 2025

---
+++ All java  practise problems/src/OopsPrime.java
@@ -0,0 +1,36 @@
+
+//  check prime number by using function ( taking nothing return nothing )
+import  java.util.*;
+public class OopsPrime {
+
+    public void Prime(){
+
+        // declare a varible
+        int count = 0;
+
+        // taking input
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int a = sc.nextInt();
+
+        for ( int i = 1; i <= a; i++){
+            if ( a % i == 0){
+                count++;
+            }
+        }
+
+        if ( count == 2){
+            System.out.println("Prime number");
+        }
+        else{
+            System.out.println("Not a Prime number");
+        }
+    }
+
+    public  static void main(String args[]){
+        // create new object of class
+        OopsPrime p = new OopsPrime();
+        p.Prime();
+    }
+}
