4cf1e3db9556cf327cd8151441d91a5f12240086
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-12 16:33:50 +0530

    12 april 2025

--- All java  practise problems/src/Point.java
+++ All java  practise problems/src/Point.java
@@ -13,6 +13,10 @@ public class Point {
         this.y = y;
         System.out.println("Point class Parameterized constructor is called");
     }
+    void showData(){
+        System.out.println("X_co: " + x);
+        System.out.println("Y_co: " + y);
+    }
     void hi(){
         System.out.println("Hii... Method is called");
 
@@ -21,4 +25,44 @@ public class Point {
 //        System.out.println("\n x=%d Y =%d",this.x,this.y);
         System.out.println("x value is: " +x);
     }
+    void Add(){
+        int x = 12;
+        int y = 10;
+        System.out.println("Addition is: " + (x +y));
+    }
+}
+
+// child class
+class Circle1 extends Point{
+    float r;
+    int z = 102;
+    public  Circle1(){
+        System.out.println("circle class Default constructor is called");
+
+    }
+
+    public  Circle1(int x , int y , float r){
+        super(x,y);// call parent class constructor
+        this.r=r;
+        System.out.println("circle class parameterized constructor is called");
+
+    }
+    void  showData(){
+        super.hi(); // call parent class member function
+        System.out.println("X_co: " + x);
+        super.Add();
+        System.out.println("Y_co: " + y);
+        System.out.println("Radius:  is: " + r);
+        System.out.println("circle z: " + z);
+        System.out.println("Point class z: " + super.z);
+
+    }
+    public static void main(String args[]){
+        Circle1 c1 = new Circle1(11,22,5.6f);
+        c1.showData();
+        c1.Add();
+
+    }
+
+
 }
