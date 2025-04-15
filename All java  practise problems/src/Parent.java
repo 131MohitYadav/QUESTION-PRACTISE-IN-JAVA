c1109c9a2442a821103033fe4a4bd80dd490e4ac
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-15 08:55:50 +0530

    15 april 2025

---
+++ All java  practise problems/src/Parent.java
@@ -0,0 +1,24 @@
+// METHOD OVERRIDING EXAMPLE
+public class Parent {
+    public void show() {
+        System.out.println("This is parent class show method......");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    public void show() {
+        System.out.println("This is child class show method.....");
+    }
+
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        p.show(); // Will call Parent's show()
+
+        Child c = new Child();
+        c.show(); // Will call Child's show()
+
+        Parent pc = new Child();
+        pc.show(); // Will call Child's show() due to overriding (runtime polymorphism)
+    }
+}
