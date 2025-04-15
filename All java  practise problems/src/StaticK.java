64678b17d8d664a0dff0b2b875b6f199043ff021
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-15 09:33:25 +0530

    15 april 2025

---
+++ All java  practise problems/src/StaticK.java
@@ -0,0 +1,22 @@
+
+/// static keyword
+public class StaticK {
+
+    // instace class or member data
+    int x;
+    int y;
+    int count;
+
+    // default constructor
+    public StaticK(){
+        count++;
+    }
+    public static void main(String args[]){
+        StaticK sk1 = new StaticK();
+       // System.out.println(sk.count); // access by classname.memberdata name
+        StaticK sk2 = new StaticK();
+        StaticK sk3 = new StaticK();
+        StaticK sk4 = new StaticK();
+        System.out.println(sk3.count);
+    }
+}
