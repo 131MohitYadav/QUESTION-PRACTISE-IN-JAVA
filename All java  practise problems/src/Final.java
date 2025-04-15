7671c9a70f5ddc90a390d45f7e33a403e709f348
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-15 09:10:08 +0530

    15 april 2025

--- All java  practise problems/src/Final.java
+++ All java  practise problems/src/Final.java
@@ -3,14 +3,17 @@
 
 public class Final {
 
-    public final void show(){
+    final int max = 10;
+    public void show(){
+
+
         System.out.println("This is parent class show method...");
     }
 }
 
 class  Child1 extends Final{
     public void show(){
-        System.out.println("This is child class show method..." + );
+        System.out.println("This is child class show method..." + super.max);
     }
     public static void main(String args[]){
         Final f = new Child1();
