132328d3f1896c59dd975248d7c078c0d19c135a
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-16 10:40:29 +0530

    16 april 2025

--- All java  practise problems/src/Point5.java
+++ All java  practise problems/src/Point5.java
@@ -18,11 +18,20 @@ public class Point5 {
     public static void main(String args[]){
 
         Point5 p1 = new Point5();
+        Point5 p2 = new Point5();
+        Point5 p3 = new Point5();
+        Point5 p4 = new Point5();
 
         // how access method
         // classname.methodname
 
         System.out.println(p1.count);
         System.out.println("Number of object is created: " + count);
+        System.out.println("Number of object is created: " + Point5.count);
+
+        System.out.println("static method is called ......");
+        System.out.println("No. of object is created : " + p1.getCtr());
+        System.out.println("No. of object is created : " + Point5.getCtr());
+
     }
 }
