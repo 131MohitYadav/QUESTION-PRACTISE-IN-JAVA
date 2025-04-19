cfa4b6cf8c3bf37b068002610dba34d2ff78e449
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-19 09:00:57 +0530

    19 april 2025

--- All java  practise problems/src/Test1.java
+++ All java  practise problems/src/Test1.java
@@ -19,5 +19,14 @@ public class Test1 {
 
     public static void add ( int a ,  int b , int d , int e){
         int c = a + b + d + e;
+        System.out.println("Additon:  "  + c);
+    }
+
+    // variable Argument
+    public static  void main(String...args){
+        Test1 t1 = new Test1();
+        t1.add();
+        t1.add(2, 3);
+        t1.add(1,3,3,3);
     }
 }
