360abf00d18a9b561c79b91e62ad1c48f6c7009e
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-21 10:23:12 +0530

    21 april 2025

---
+++ All java  practise problems/src/Rgpv.java
@@ -0,0 +1,16 @@
+abstract  class Rgpv {
+
+    public abstract void lab();
+    public abstract  void cctv();
+    public  void copyCheck(){
+        System.out.println("This is copycheck method");
+
+    }
+    // college is child class
+    abstract  class College extends Rgpv{
+
+
+    }
+
+
+}
