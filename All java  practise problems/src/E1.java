ab26f6f49a6ab84de3285bfef603893dd7b0ec09
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-25 11:13:38 +0530

    25 april 205

---
+++ All java  practise problems/src/E1.java
@@ -0,0 +1,19 @@
+// example of EXCEPTION HANDLING
+
+public class E1 {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello...Mohit");
+        int a , b, c;
+        a = 10;
+        b = 0;
+        System.out.println("Before Exception"); // it give arithematic excpetion is a checked exception
+        c = a/b;
+        System.out.println(c);
+        System.out.println("Hello..end");
+        System.out.println("After excepting");
+
+    }
+
+}
