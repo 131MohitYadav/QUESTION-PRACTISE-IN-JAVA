3315dd76d415ce31d2e0bd475e0f7cebcf0b8608
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-26 20:51:12 +0530

    26 april 2025

--- All java  practise problems/src/E2.java
+++ All java  practise problems/src/E2.java
@@ -5,6 +5,21 @@ public class E2 {
     public static void main(String args[]){
 
 
+        System.out.println("Hello...hi");
+        int a, b , c = 0;
+        a = 10;
+        b = 2;
+        System.out.println("Before try Exception");
+        try{
+            System.out.println("Enter try");
+            c = a/b;
+
+            System.out.println("Exit try");
+        }catch(ArithmeticException ae){
+            System.out.println("This is Catch block");
+        }
+        System.out.println(c);
+
 
     }
 }
