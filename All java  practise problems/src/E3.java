73598e348fd0e218b3cf8aed1d5660db559d0ed6
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-28 18:25:22 +0530

    28 april 2025

--- All java  practise problems/src/E3.java
+++ All java  practise problems/src/E3.java
@@ -9,6 +9,18 @@ public class E3 {
  a = 10;
   b = 2;
         System.out.println("before exception..");
+        try{
+
+            System.out.println("Enter Try");
+            c = a / b;
+            System.out.println("Exit try");
+        }finally {
+            System.out.println("This is finally block");
+
+            System.out.println(c);
+            System.out.println("After Exception");
+
+        }
 
     }
 }
