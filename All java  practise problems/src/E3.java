5273316226c2d3783041a18109f5dcd721e5bd81
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-28 18:21:57 +0530

    28 april 2025

---
+++ All java  practise problems/src/E3.java
@@ -0,0 +1,14 @@
+
+// Example of Exception by using finally block
+
+public class E3 {
+
+    public static void main(String args[]){
+
+        int a , b , c = 0;
+ a = 10;
+  b = 2;
+        System.out.println("before exception..");
+
+    }
+}
