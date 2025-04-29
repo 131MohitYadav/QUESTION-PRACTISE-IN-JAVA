e5b48accfdf826b773531e073d403b6e84fc155f
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-29 11:56:59 +0530

    29 april 2025

---
+++ All java  practise problems/src/E4.java
@@ -0,0 +1,20 @@
+
+
+// command line argument -> example pass information at the time of execution of the program
+
+public class E4 {
+
+    public static void main(String args[]){
+
+        int n = 0;
+        try{
+            n = Integer.parseInt(args[0]);
+
+        } catch(ArrayIndexOutOfBoundsException ae){
+            System.out.println("Please enter at east on ARgument from command line");
+        }
+        catch (NumberFormatException ne){
+            System.out.println();
+        }
+    }
+}
