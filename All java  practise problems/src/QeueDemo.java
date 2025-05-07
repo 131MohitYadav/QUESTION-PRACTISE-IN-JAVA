23f56ce065e3e5ee9728452f4cf278588013d6af
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-07 17:14:57 +0530

    7 may 2025

--- All java  practise problems/src/QeueDemo.java
+++ All java  practise problems/src/QeueDemo.java
@@ -13,37 +13,48 @@ public class QeueDemo {
 
     // enque operation peform
 
-    public static void enq(int data){
+    public static void enq(int data) {
         // step 1 -> check overflow condition
-        if(isFull()){
+        if (isFull()) {
             System.out.println("Queue is overflow");
         }
 
         // step 3 -> check if queue is empty increase front and rear by 1;
-        if ( isEmpty()){
+        if (isEmpty()) {
             System.out.println("Queue is empty");
             front++;
             rear++;
 
-        }
-        else{
+        } else {
             // if queue is not empety than rear increase by 1
             rear++;
             // step 4 -> insert data into queue
             Q[rear] = data;
             System.out.println("DAta insert into queue");
         }
-
+    }
         public static void display(){
             if ( isEmpty()){
                 System.out.println("Queue is empty");
             }
             else{
                 System.out.println("Elements of the Queue");
+                for ( int i = front; i<= rear; i++){
+                    System.out.println("-->" + Q[i]);
+
+                }
             }
         }
 
-    }
+        public static void main (String args[]){
+
+        enq(10);
+        enq(20);
+        enq(30);
+        enq(40);
+        display();
+        }
+
 
 
 }
