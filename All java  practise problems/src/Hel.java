518ff38ad482dea830f38d2a392569c9a82ce08c
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-16 20:11:21 +0530

    16 may 2025

--- All java  practise problems/src/Hel.java
+++ All java  practise problems/src/Hel.java
@@ -3,5 +3,6 @@ public class Hel {
         System.out.println("Hello Mohit Yadav");
         System.out.println("Hello Rohit Yadaav");
         System.out.println("heljls");
+        System.out.println("helsf;safj things to do");
     }
 }
