c831ac6a56d19fca7e101d8ce2b3de7efad5c55e
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-17 21:07:08 +0530

    17 may 2025

--- All java  practise problems/src/Hel.java
+++ All java  practise problems/src/Hel.java
@@ -5,9 +5,13 @@ public class Hel {
         int y = 3;
 
         int sum = x + y;
-        int sub =  - y;
+        int sub = x - y;
+        int div = x % y;
+
+
         System.out.println(sum);
         System.out.println(sub);
+        System.out.println(div);
 
     }
 }
