dd2fa723055139907fd973b6696de71b6ce895af
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-17 21:05:09 +0530

    17 may 2025

--- All java  practise problems/src/Hel.java
+++ All java  practise problems/src/Hel.java
@@ -1,6 +1,10 @@
 public class Hel {
     public static void main (String args[]){
         System.out.println("Hello Mohit Yadav");
+        int x  = 23;
+        int y = 3;
+
+        int sum = x + y;
 
     }
 }
