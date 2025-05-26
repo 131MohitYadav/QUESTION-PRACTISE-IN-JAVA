d3e5d2d8cd7cf3a88f139401ee09e538b1873bd2
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-26 21:14:24 +0530

    26 may 2205

--- All java  practise problems/src/Hel.java
+++ All java  practise problems/src/Hel.java
@@ -19,6 +19,7 @@ public class Hel {
         System.out.println( 2 -3 );
         System.out.println(3 * 2);
         System.out.println(3  * 9);
+        System.out.println(3/3);
         System.out.println("Hello Rohit Yadav");
 
 
