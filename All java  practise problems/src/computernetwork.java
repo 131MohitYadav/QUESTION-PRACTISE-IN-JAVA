a4d80e2ea343076af98fe0bc924073bdfe56c207
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-05 21:37:42 +0530

    5 june 2025

--- All java  practise problems/src/computernetwork.java
+++ All java  practise problems/src/computernetwork.java
@@ -2,5 +2,8 @@ public class computernetwork {
 
     public static void main ( String args[]){
 
+        System.out.println("UDP -> user data gram header 3 bit fixed size is called user datagram " +
+                "souce ");
+
     }
 }
