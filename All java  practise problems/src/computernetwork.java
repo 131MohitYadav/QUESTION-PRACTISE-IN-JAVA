cbe371882e03f1cb26ef3923a5f287bc450897b1
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-05 21:40:47 +0530

    5 june 2025

--- All java  practise problems/src/computernetwork.java
+++ All java  practise problems/src/computernetwork.java
@@ -3,7 +3,8 @@ public class computernetwork {
     public static void main ( String args[]){
 
         System.out.println("UDP -> user data gram header 3 bit fixed size is called user datagram " +
-                "souce ");
+                "source port number");
+        System.out.println("tcp transaction  control protocol");
 
     }
 }
