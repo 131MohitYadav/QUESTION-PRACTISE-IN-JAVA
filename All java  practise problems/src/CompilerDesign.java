cb72d3b831cb278ae687a9ae41d7d82871a57812
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-12 19:45:45 +0530

    12 june 2025

--- All java  practise problems/src/CompilerDesign.java
+++ All java  practise problems/src/CompilerDesign.java
@@ -3,7 +3,16 @@ public class CompilerDesign {
 
         System.out.println( " anyleyeze the syntax anylaize3r " +
                 "there are two ways -> " +
-                "1. Error reporting/ handling");
+                "1. Error reporting/ handling" +
+                "lecical erron" +
+                "syntactiv error" +
+                "semanctic error" +
+                "logical error" +
+                "2. Error recovery" +
+                "panic mode" +
+                "phrase  levele" +
+                "error production" +
+                "globsl correction");
 
     }
 }
