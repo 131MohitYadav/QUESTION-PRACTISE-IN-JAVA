6250fcdf5d26c8feed471490b8f855005df0f2c7
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-12 19:41:22 +0530

    12 june 2025

--- All java  practise problems/src/CompilerDesign.java
+++ All java  practise problems/src/CompilerDesign.java
@@ -1,5 +1,9 @@
 public class CompilerDesign {
     public static void main(String args[]){
 
+        System.out.println( " anyleyeze the syntax anylaize3r " +
+                "there are two ways -> " +
+                "1. Error reporting/ handling");
+
     }
 }
