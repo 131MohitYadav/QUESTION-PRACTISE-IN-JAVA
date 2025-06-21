c9111fa9254d7f35018d4dbe6f1e074465648349
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-21 19:08:19 +0530

    21 june 2025

--- All java  practise problems/src/macbook.java
+++ All java  practise problems/src/macbook.java
@@ -1,5 +1,7 @@
 public class macbook {
     public static void main ( String args[]){
         System.out.println("helo sirir");
+        System.out.println("what are you doing");
+        System.out.println("What the fuck");
     }
 }
