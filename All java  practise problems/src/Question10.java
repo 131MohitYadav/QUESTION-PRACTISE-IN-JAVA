247e42d780d0d55a630be273deca68d2eb508fac
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-23 22:28:44 +0530

    23 june 2025

--- All java  practise problems/src/Question10.java
+++ All java  practise problems/src/Question10.java
@@ -1,24 +1,14 @@
 public class Question10 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int a = 22;
         int b = 23;
         int c = 25;
-        int d = a + b + c /3;
+        int d = a + b + c / 3;
         int e = a * b + c;
-        int f =  a * b;
+        int f = a * b;
         System.out.println("AVERAGE OF THREE NUMBER IS: " + d);
         System.out.println("The value of E is : " + e);
         System.out.println("The value of F is ; " + f);
         System.out.println("Addition of number : ");
-        System.out.println("Netbeans ide 8.2 version ");
-        System.out.println("Jdk 1.8.2 hello version");
-        System.out.println("After that install jar file  to setup Netbeans");
-        System.out.println("Hello Mohit Yadav");
-        System.out.println("What are you doing");
-        System.out.println("hii MOhit Yada wha neteand ide adown version 24");
-        System.out.println("Wre a java to find oute primer number or factorization");
-        System.out.println("wreiat a progra to find palindrome string");
-        System.out.println("wrrit a progr to find highest number and lowerst ");
-        System.out.println("write to find the largest and second lages number");
     }
 }
