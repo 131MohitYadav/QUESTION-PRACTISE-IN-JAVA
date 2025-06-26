0be2a27ff646746a4df8037dcb0a8c18914c849a
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-26 19:26:31 +0530

    26 june 2025

--- All java  practise problems/src/Question10.java
+++ All java  practise problems/src/Question10.java
@@ -7,10 +7,12 @@ public class Question10 {
         int e = a * b + c;
         int f = a * b;
         int g = a * b + c;
+        int h = a / b * c + b - c;
         System.out.println("AVERAGE OF THREE NUMBER IS: " + d);
         System.out.println("The value of E is : " + e);
         System.out.println("The value of F is ; " + f);
         System.out.println("Addition of number : ");
+        System.out.println("The value of h is: " + h);
 
     }
 }
