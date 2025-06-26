ca33fed24fdcfd3f97c73946f3d6967d85b07aa1
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-26 19:37:36 +0530

    26 june 2025

--- All java  practise problems/src/Question10.java
+++ All java  practise problems/src/Question10.java
@@ -10,6 +10,7 @@ public class Question10 {
         int h = a / b * c + b - c;
         int i = a * c;
         int j = a * b * c;
+        int k = a * b * c * d;
         System.out.println("AVERAGE OF THREE NUMBER IS: " + d);
         System.out.println("The value of E is : " + e);
         System.out.println("The value of F is ; " + f);
@@ -17,6 +18,7 @@ public class Question10 {
         System.out.println("The value of h is: " + h);
         System.out.println("The value of i is: " + i);
         System.out.println("The value of j is: " + j);
+        System.out.println("The value of  k is: " + k);
 
     }
 }
