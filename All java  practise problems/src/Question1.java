486bf4a45a8cb3c4082cf502568fbd09c2f34b93
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-27 18:55:42 +0530

    27 june 2025

--- All java  practise problems/src/Question1.java
+++ All java  practise problems/src/Question1.java
@@ -1,6 +1,7 @@
 public class Question1{
     public  static void main(String [] args){
         System.out.println("Hello\nAlexandra Abramove");
+        System.out.println("hello Mohit \n Rohit Yadav");
 
     }
 }
