2d96a52a7539d3aa1f9804d6ca0d5838f729cce9
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-27 18:56:48 +0530

    27 june 2025

--- All java  practise problems/src/Question1.java
+++ All java  practise problems/src/Question1.java
@@ -2,6 +2,7 @@ public class Question1{
     public  static void main(String [] args){
         System.out.println("Hello\nAlexandra Abramove");
         System.out.println("hello Mohit \n Rohit Yadav");
+        System.out.println("Nothing to entry to show that");
 
     }
 }
