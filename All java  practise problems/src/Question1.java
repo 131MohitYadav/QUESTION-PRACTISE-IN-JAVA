0cac07eac1c8adc857411c512fb767b062944c3a
131MohitYadav <yadavjimohit54@gmail.com>
2025-06-27 18:59:15 +0530

    27 june 2025

--- All java  practise problems/src/Question1.java
+++ All java  practise problems/src/Question1.java
@@ -3,6 +3,10 @@ public class Question1{
         System.out.println("Hello\nAlexandra Abramove");
         System.out.println("hello Mohit \n Rohit Yadav");
         System.out.println("Nothing to entry to show that");
+        int x = 3;
+        int y = 3;
+        System.out.println("Ths sum of : " + (x + y));
+
 
     }
 }
