b82ec27fc61280dbadb007ff95f2eb7e4d1f88da
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-09 11:49:45 +0530

    9 july 2025

---
+++ All java  practise problems/src/EncapsulationGetter.java
@@ -0,0 +1,12 @@
+public class EncapsulationGetter {
+
+    private String name ="Mohit yadav";
+    private String enroll = "012012";
+    public String getName(){
+        return name;
+    }
+
+    public String getEnroll(){
+        return enroll;
+    }
+}
