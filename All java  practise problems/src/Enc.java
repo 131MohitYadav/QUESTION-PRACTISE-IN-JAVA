5531e6ecdbf47ddcd52129478fc92f67e3758c8f
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-09 11:55:36 +0530

    9 july 2025

--- All java  practise problems/src/Enc.java
+++ All java  practise problems/src/Enc.java
@@ -1,5 +1,14 @@
 public class Enc {
     public static void main(String args[]){
 
+        EncapsulationGetter eg = new EncapsulationGetter();
+        System.out.println("Name : " +eg.getName());
+        System.out.println("Enroll : " + eg.getEnroll());
+        eg.setName("Rohit yadav");
+        eg.setEnroll("025142");
+        System.out.println("Name is : " + eg.getName());
+        System.out.println("Enroll is: " + eg.getEnroll());
+
+
     }
 }
