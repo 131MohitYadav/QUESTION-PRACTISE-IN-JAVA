dac3eb32536adea083c267150106d3f1cf33cb4d
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-11 21:43:48 +0530

    11 july 2025

--- All java  practise problems/src/Enc.java
+++ All java  practise problems/src/Enc.java
@@ -8,8 +8,6 @@ public class Enc {
         eg.setEnroll("025142");
         System.out.println("Name is : " + eg.getName());
         System.out.println("Enroll is: " + eg.getEnroll());
-        System.out.println("Hello MOhit ja  ");
-        System.out.println("Hii MOhit Yada");
 
 
 
