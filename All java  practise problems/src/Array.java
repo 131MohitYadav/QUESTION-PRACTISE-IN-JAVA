3383900ec4876aa53cfc8f260ba387c1843fa7b6
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-16 20:09:33 +0530

    16 july 2025

--- All java  practise problems/src/Array.java
+++ All java  practise problems/src/Array.java
@@ -3,5 +3,9 @@ public class Array {
     public static  void main(String args [ ]){
 
         int rollno [] = new int [5];
+        int hello[] = new int[2];
+
+        int x = [23, 23 , 23 , 23];
+        System.out.println(x);
     }
 }
