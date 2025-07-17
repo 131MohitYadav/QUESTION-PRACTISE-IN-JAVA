c6961c8a601e055b823fd52491231c85ab3b961a
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-17 21:41:21 +0530

    11 july 2025

--- All java  practise problems/src/Array.java
+++ All java  practise problems/src/Array.java
@@ -5,12 +5,10 @@ public class Array {
         int rollno [] = new int [5];
         int hello[] = new int[2];
 
-        int x = [23, 23 , 23 , 23];
-        int y = [23 , 23, 23, 2];
-        System.out.println(x);
         System.out.println(rollno[23]);
         System.out.println(rollno[23]);
-        System.out.println("helol MOhit yadav");
-        System.out.println("Not a Mohit Yadav wel com to bhipal nothing to ");
+        System.out.println(rollno[23]);
+
+
     }
 }
