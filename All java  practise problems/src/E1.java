a16b4ba998d75483f1fb25a100b1d0e565b8b064
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-22 20:38:29 +0530

    22 july 2025

--- All java  practise problems/src/E1.java
+++ All java  practise problems/src/E1.java
@@ -8,12 +8,14 @@ public class E1 {
         int a , b, c;
         a = 10;
         b = 0;
+        int d = 23;
         System.out.println("Before Exception"); // it give arithematic excpetion is a checked exception
         c = a/b;
         System.out.println(c);
         System.out.println("Hello..end");
         System.out.println("After excepting");
         System.out.println("Hello Mohit Yadav");
+        System.out.println(d * a * b);
 
     }
 
