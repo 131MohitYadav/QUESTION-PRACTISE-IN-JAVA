aee703686387bf3d87b8ddd4919fa3a2a5c5d733
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-24 21:13:14 +0530

    24 july 2025

--- All java  practise problems/src/CheckgivenStringisuppercaseornot.java
+++ All java  practise problems/src/CheckgivenStringisuppercaseornot.java
@@ -9,9 +9,7 @@ public class CheckgivenStringisuppercaseornot {
         System.out.println("ENTER ANY STRING: ");
         String s = sc.nextLine();
 
-        System.out.println("Here it is UPpercase string entering string");
-        System.out.println("Hello Mohit Yadav welcoe to ");
-        System.out.println("Hello what to do nothing to worry that");
+
         System.out.println(s.toUpperCase().equals(s));
     }
 
