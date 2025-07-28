3c77568ec097ce7a73d6c882806e1839a90765d5
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-28 22:51:02 +0530

    28 july 2025

--- All java  practise problems/src/Array1.java
+++ All java  practise problems/src/Array1.java
@@ -22,6 +22,7 @@ public static  void main (String args[]){
     System.out.println(enrollno[4]);
     System.out.println(enrollno[5]);
     System.out.println(enrollno[5]);
+    System.out.println(enrollno[7]);
 
 
   }
