5a40609aebee8da3d5f5ab36907144aabf4c957e
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-30 22:21:10 +0530

    30 july 2025

--- All java  practise problems/src/CheckgivenStringisuppercaseornot.java
+++ All java  practise problems/src/CheckgivenStringisuppercaseornot.java
@@ -14,6 +14,7 @@ public class CheckgivenStringisuppercaseornot {
         System.out.println("Hell mohti aydaf");
         System.out.println("hello mohit yadav");
         System.out.println("Helo waht are you dooing");
+        System.out.println("hello waht are you doing");
     }
 
 }
