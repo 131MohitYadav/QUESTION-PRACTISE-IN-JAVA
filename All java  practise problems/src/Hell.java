5f07325a3e20ff91b19b8ab63009b66aa88c32fc
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-30 22:32:03 +0530

    30 july 2025

--- All java  practise problems/src/Hell.java
+++ All java  practise problems/src/Hell.java
@@ -4,5 +4,9 @@ public class Hell {
         System.out.println("Nothing to do");
         String s = "MOhit Yadav";
         System.out.println(s);
+        int a = 23;
+        int x = 2;
+        int sum = a + x;
+        System.out.println(sum);
     }
 }
