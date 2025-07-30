24e4a6584386e125cbdd538d6962b97f1d89df77
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-30 22:27:50 +0530

    30 july 2025

--- All java  practise problems/src/Hell.java
+++ All java  practise problems/src/Hell.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Hell {
+    public static  void main(String args[]){
+        System.out.println("helo Mohit Yadav");
+        System.out.println("Nothing to do");
+        String s = "MOhit Yadav";
+        System.out.println(s);
+    }
 }
