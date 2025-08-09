b2e0292cd119e9995238d3ede7ecb8fba69a65dd
131MohitYadav <yadavjimohit54@gmail.com>
2025-08-09 12:30:52 +0530

    9 august 25

--- All java  practise problems/src/AddingValue.java
+++ All java  practise problems/src/AddingValue.java
@@ -6,6 +6,7 @@ public class AddingValue {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("ENTER TWO VALUE: ");
+        System.out.println("ENTER YOUR NAME: ");
         BigDecimal bg1 = sc.nextBigDecimal();
         BigDecimal bg2 = sc.nextBigDecimal();
         System.out.println("Result is: " + bg1.subtract(bg2));
