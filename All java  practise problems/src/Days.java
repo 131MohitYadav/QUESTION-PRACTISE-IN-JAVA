9a1439e23372c644cbc80cf0dd351ec0c8dea84d
131MohitYadav <yadavjimohit54@gmail.com>
2025-08-21 12:30:57 +0530

    21 aug 2025

---
+++ All java  practise problems/src/Days.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Days {
+
+    public static void mian(String args[]){
+
+        // delcare a variable
+        int Y , M , D, TotalDays, RemainingDays;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER NUMBER OF DAYS : ");
+        TotalDays = sc.nextInt();
+    }
+}
