f21a346edd8a463c228292f651bc1417ff2e69a7
131MohitYadav <yadavjimohit54@gmail.com>
2025-08-21 12:04:39 +0530

    21 aug 2025

--- All java  practise problems/src/Time.java
+++ All java  practise problems/src/Time.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Time {
 
     public static void main(String args[]){
 
         // delcare a variable
         int sec,min,hr,Totalseconds;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF SECONDS: ");
+        Totalseconds = sc.nextInt();
+
+        // peform the operation
+
+        hr = Totalseconds/3600;
+        min = (Totalseconds % 3600)/60;
+        sec = Totalseconds % 60;
+
+        System.out.println("H.M.S - " + hr + ":" + min + ":" + sec);
     }
 }
