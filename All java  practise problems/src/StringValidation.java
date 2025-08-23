06b6f5fbddb60c0f16a8fed6189087fbe865f2b5
131MohitYadav <yadavjimohit54@gmail.com>
2025-08-23 12:35:08 +0530

    23 aug 2025

---
+++ All java  practise problems/src/StringValidation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StringValidation {
+
+    public static  void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = sc.nextLine();
+
+        if(input.length() < 3){
+            System.out.println("Error: the string ust have at least 3 characters");
+        }
+        else if(!input.matches("[a-zA-Z]+")){
+            System.out.println("Error: The string must contain only alphabets");
+
+        }
+        else{
+            System.out.println("Valid String: " + input);
+        }
+    }
+}
