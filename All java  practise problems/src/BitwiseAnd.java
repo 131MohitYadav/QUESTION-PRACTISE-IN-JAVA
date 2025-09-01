09c38c0483ae6ab2a26cdc7a7ef93324274396ac
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-01 20:16:04 +0530

    1 aug 2025

--- All java  practise problems/src/BitwiseAnd.java
+++ All java  practise problems/src/BitwiseAnd.java
@@ -3,14 +3,17 @@ public class BitwiseAnd {
 
     public static void main(String args[]){
 
-        int x , y , z ,a , b ,c ;
+        int x , y , z ,a , b ,c , d;
         x = 7;
         y = 5;
         a = 2;
         b = 4;
         c = 2 | 4;
         z = x & y;
+        d = x + y;
+
         System.out.println(z);
         System.out.println(c);
+        System.out.println(d);
     }
 }
