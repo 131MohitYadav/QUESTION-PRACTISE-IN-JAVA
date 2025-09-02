4a56d95472a10bcae450f380a61966490b98a97c
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-02 20:56:04 +0530

    2 aug 2025

---
+++ All java  practise problems/src/Op1.java
@@ -0,0 +1,10 @@
+public class Op1 {
+
+    public static void main(String args[]){
+
+        int a , b;
+        a = 23;
+        b = 32;
+        System.out.println("Before Swapping: ");
+    }
+}
