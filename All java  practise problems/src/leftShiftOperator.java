b7f9b5a37578226c7bd63ae77e233217424291c1
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-04 10:13:29 +0530

    3 aug 2025

--- All java  practise problems/src/leftShiftOperator.java
+++ All java  practise problems/src/leftShiftOperator.java
@@ -1,5 +1,10 @@
 public class leftShiftOperator {
     public static void main(String args[]){
 
+        int x , y;
+        x = 128;
+        y = x << 3;
+        System.out.println(y);
+
     }
 }
