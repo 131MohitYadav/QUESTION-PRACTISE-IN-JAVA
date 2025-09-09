a18c6c59a0fb390fe2f02ea6d718486fc2353e9e
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-09 20:17:13 +0530

    9 sep 2025

--- All java  practise problems/src/LogicalOrOperator.java
+++ All java  practise problems/src/LogicalOrOperator.java
@@ -2,12 +2,19 @@ public class LogicalOrOperator {
     public static void main(String args[]){
 
         int a , b , c , d;
+        int x , y , z , w;
+        x = 7;
+        y = 3;
+        z = x & y;
+        w = x ^ y;
         a = 10;
         b = 20;
         c = 30;
         d = 40;
 
         System.out.println( ! ( a > b ||  c< d));// false
+        System.out.println( z);
+        System.out.println(w);
 
 
     }
