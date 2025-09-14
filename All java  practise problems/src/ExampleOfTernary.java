26595c535f3218988a36e97bf60d845fd8e3196a
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-14 12:54:04 +0530

    14 sep 2025

--- All java  practise problems/src/ExampleOfTernary.java
+++ All java  practise problems/src/ExampleOfTernary.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ExampleOfTernary {
     public static void main (String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        S resutl;
+
+        System.out.println("ENTER ANY NUMBER : ");
+        int n = sc.nextInt();
+
+         resutl = n % 2 == 0 ? "EVen Number" : "Odd Number";
+        System.out.println(resutl);
 
     }
 }
