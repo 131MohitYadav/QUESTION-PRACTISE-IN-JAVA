536e4a7ca14dfc3e6b8c44bc7d6ae4992d5ad4fe
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-21 15:01:47 +0530

    21 sep 2025

--- All java  practise problems/src/ExampleOfTernary.java
+++ All java  practise problems/src/ExampleOfTernary.java
@@ -28,7 +28,10 @@ public class ExampleOfTernary {
         int n = sc.nextInt();
 
         if ( n % 2 == 0){
-            System.out.println("Hello Mohit Yadav");
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
         }
 
 
