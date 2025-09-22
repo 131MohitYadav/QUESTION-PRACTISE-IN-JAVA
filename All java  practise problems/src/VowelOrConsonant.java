9f3df8678715a3ba93604e424eaba0a154d5ebdf
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-22 20:30:49 +0530

    22 sep 2025

--- All java  practise problems/src/VowelOrConsonant.java
+++ All java  practise problems/src/VowelOrConsonant.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class VowelOrConsonant {
 
     public static void main(String args[]){
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter any character: ");
+        char c = sc.next().charAt(0);
+
+
     }
 }
