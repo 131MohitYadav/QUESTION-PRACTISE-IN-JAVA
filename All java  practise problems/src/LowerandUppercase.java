405cb07ea27d135f3e6baaf27172337480101879
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-23 12:05:21 +0530

    23 sep 25

--- All java  practise problems/src/LowerandUppercase.java
+++ All java  practise problems/src/LowerandUppercase.java
@@ -1,5 +1,22 @@
 
 // write a java program to check give string is uppercase or lowercase without using logical operator or build in function
 
+import java.util.Scanner;
+
 public class LowerandUppercase {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any Character: ");
+        char c  = sc.next().charAt(0);
+
+        if ( c >= 'a' && c <= 'z'){
+            System.out.println("Lower case");
+        }
+        else {
+            System.out.println("Upper case");
+        }
+    }
 }
