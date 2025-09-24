51ae137da15e4228229046f28042baa32ffa0f6a
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-25 04:02:31 +0530

    25 sep 2025

--- All java  practise problems/src/DecimalCompare.java
+++ All java  practise problems/src/DecimalCompare.java
@@ -8,5 +8,19 @@ public class DecimalCompare {
         double n1, n2;
         System.out.println("Enter first floating number: ");
         n1 = sc.nextDouble();
+
+        System.out.println("Enter Second floating number: ");
+        n2  = sc.nextDouble();
+
+        n1 = Math.round(n1 * 1000.0) / 1000.0;
+        n2 = Math.round(n2 * 1000.0) / 1000.0;
+
+        if ( n1 == n2){
+            System.out.println("Ther are same");
+        }
+        else{
+            System.out.println("They are different");
+        }
+
     }
 }
