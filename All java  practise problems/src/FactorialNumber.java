9cc56fd02f3d67f669a35a61867c4c66350e432b
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-24 10:54:12 +0530

    24 sep 25

--- All java  practise problems/src/FactorialNumber.java
+++ All java  practise problems/src/FactorialNumber.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class FactorialNumber {
     public static void main(String args[]){
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        int fact = 1;
 
+        for ( int i = n; i >= 1; i--){
+            fact = fact * i;
+
+        }
+        System.out.println("Factorial of Given number: ");
     }
 }
