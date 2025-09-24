69e8e4f2dca1779b9dcec0623cb19f6f081abd23
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-25 03:58:57 +0530

    25 sep 2025

---
+++ All java  practise problems/src/DecimalCompare.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DecimalCompare {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        double n1, n2;
+        System.out.println("Enter first floating number: ");
+        n1 = sc.nextDouble();
+    }
+}
