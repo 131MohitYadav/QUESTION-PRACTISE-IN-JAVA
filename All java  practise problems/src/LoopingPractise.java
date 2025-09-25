e12078256563e7cb93555cf216d993550c2ac2e9
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-25 22:35:22 +0530

    25 sep 25

---
+++ All java  practise problems/src/LoopingPractise.java
@@ -0,0 +1,40 @@
+public class LoopingPractise {
+    public static void main (String args[]){
+
+        // write a java program to print number from 1 to 5 using while , do while and for loop
+
+//        // example for loop
+//        for ( int i = 1; i <= 5; i++){
+//            System.out.println(i);
+//        }
+
+
+        // EXAMPLE FOR WHILE LOOP
+
+        // initalizatin
+//         int i = 1;
+//
+//         // test condidtion
+//         while ( i <= 5){
+//             // statemetn
+//             System.out.println(i);
+//             // incrment or dccrment
+//             i++;
+//         }
+
+
+        // EXAMPLE FOR DO WHILE LOOP
+
+        // initilaiztion
+
+        int a = 1;
+        do {
+            // statement
+            System.out.println(a);
+            // incrmenet or dccrment
+            a++;
+        }
+        // test codnition
+        while ( a <= 5);
+    }
+}
