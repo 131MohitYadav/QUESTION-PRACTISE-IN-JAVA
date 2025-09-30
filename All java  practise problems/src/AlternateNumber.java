cf7435a53fc7622d923e12886ff3d0c6703b01d1
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-30 06:19:57 +0530

    30 sep 25

--- All java  practise problems/src/AlternateNumber.java
+++ All java  practise problems/src/AlternateNumber.java
@@ -1,8 +1,10 @@
 public class AlternateNumber {
     public static void main(String args[]){
 
-        for ( int i = 80; i >= 70 ; i--){
-            System.out.println();
+        for ( int i = 80; i >= 70 ;i--){
+            System.out.println(i);
+            System.out.println("Skip");
+
         }
     }
 }
