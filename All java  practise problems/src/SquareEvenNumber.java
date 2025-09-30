10e357ace015b0368eb568a3722a36db3ced5224
131MohitYadav <yadavjimohit54@gmail.com>
2025-09-30 06:47:00 +0530

    30 sep 25

---
+++ All java  practise problems/src/SquareEvenNumber.java
@@ -0,0 +1,10 @@
+public class SquareEvenNumber {
+    public static void main(String args[]){
+
+        for(int i = 10; i <= 20; i++){
+            if ( i % 2 == 0){
+                System.out.println("The Square of Even number: " + (i * i));
+            }
+        }
+    }
+}
