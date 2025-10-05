0b1f9022413295289b452974b2977a62815e50e5
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-05 17:46:48 +0530

    5 oct 25

---
+++ All java  practise problems/src/ArraySandL.java
@@ -0,0 +1,30 @@
+//import java.util.Scanner;
+//
+//public class ArraySandL {
+//
+//    public static void main(String args[]){
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter the number of array: ");
+//        int n = sc.nextInt();
+//
+//        int number[] = new int[n];
+//        System.out.println("Enter " + n + " elements");
+//        for(int i = 0; i < n; i++){
+//            number[i] = sc.next();
+//
+//
+//            // LOGIC OF THAT QUESTION
+//
+//            // int smallest = numbers[0];
+//            // int largest = numbers[0];
+//
+//            // for ( int i = 1; i < numbers.length; i++) {
+//            // if ( numbers[i] < smallest)
+//            // smallest = numbers[i];
+//            // if ( numbers[i] > largest){
+//            // largest = number[i];
+//        }
+//    }
+//}
