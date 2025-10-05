733a058abbe3ba7bd3ac3f141a454c04590f1942
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-05 17:23:02 +0530

    5 oct 25

--- All java  practise problems/src/DealershipTyre.java
+++ All java  practise problems/src/DealershipTyre.java
@@ -4,5 +4,11 @@ public class DealershipTyre {
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
+        int dealership = sc.nextInt();
+        while ( dealership --> 0){
+            int cars = sc.nextInt();
+            int bikes = sc.nextInt();
+            System.out.println(cars * 4 + bikes * 2);
+        }
     }
 }
