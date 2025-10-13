6a6546214d5c8ce18a4b5aec19928fe0a476f77b
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-13 11:50:08 +0530

    13 oct 25

--- All java  practise problems/src/ReverseNum.java
+++ All java  practise problems/src/ReverseNum.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
+// WRITE A JAVA PROGRAM TO PRINT THE REVERSE NUMBER
 public class ReverseNum {
 
     public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any number: ");//131
+        int n = sc.nextInt();
+
+        int res = 0;
+        while( n != 0){
+            int r = n % 10;//13
+            res = res * 10 + r;// 9 + 13;
+            n = n/10;
+
+        }
+
+        System.out.println("The REverse number: " + res);
+
 
 
     }
