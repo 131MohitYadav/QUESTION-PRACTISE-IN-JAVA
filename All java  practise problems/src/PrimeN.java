3006e11afb67eee0ca55f5ed7f4ec29cc205d3df
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-18 18:51:23 +0530

    18 oct 25

--- All java  practise problems/src/PrimeN.java
+++ All java  practise problems/src/PrimeN.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
 
+// Write a java program to check  give number is prime or not
 public class PrimeN {
     public static  void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println();
+        System.out.println("Enter any number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+        while(n != 0){
+
+            for ( int i = n; i < n ; i++){
+                if ( n % i == 0){
+                    count++;
+                }
+            }
+
+            if ( count == 2){
+                System.out.println("This is Prime number");
+            }
+            else {
+                System.out.println("This is not prime number");
+            }
+        }
     }
 }
