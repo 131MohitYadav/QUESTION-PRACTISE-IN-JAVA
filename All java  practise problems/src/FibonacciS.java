d3f3e98aef03298535e733788307bd8b688774a1
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-30 13:04:49 +0530

    30 oct 25

--- All java  practise problems/src/FibonacciS.java
+++ All java  practise problems/src/FibonacciS.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class FibonacciS {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any number : ");
+        int n = sc.nextInt();
+
+        // declare a variable
+        int t1, t2 , t3;
+
+         t1 = 0;
+         t2 = 1;
+        System.out.print(t1 +" " + t2 + " "); // 0 1
+        for(int i = 3; i <= n ; i++){
+            t3 = t1 + t2; // 0 + 1 = 1
+            System.out.print(t3+" ");
+            t1 = t2;//1
+            t2 = t3;//1
+        }
+    }
 }
---
+++ All java  practise problems/src/exit.java
@@ -0,0 +1,11 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class exit {
+    public static void main(String args[]){
+
+        System.out.println("hello what are you doing");
+        System.exit(0);// exit function used for theh exit from the loop
+        System.out.println("Welcome to new world");
+        System.out.println("Welcome to Madhya Pradesh");
+    }
+}
