cf9c6e8b01725f2f29b94d226f22b4a6a7ac202a
131MohitYadav <yadavjimohit54@gmail.com>
2026-03-26 19:03:01 +0530

    26 march 26

--- All java  practise problems/src/Array.java
+++ All java  practise problems/src/Array.java
@@ -13,8 +13,6 @@ public class Array {
         System.out.println(hello[2]);
         System.out.println(hello[2]);
         System.out.println(hii[23]);
-        System.out.println("Hello Mohit Yadav");
-        System.out.println("jai ho bole baba");
 
 
 
