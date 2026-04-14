08516a2c51b90d7587725e6a52c19b5f9f1cd455
131MohitYadav <yadavjimohit54@gmail.com>
2026-04-14 22:42:53 +0530

    14 apr 26

---
+++ All java  practise problems/src/StringValidation1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class StringValidation1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+    }
+}
