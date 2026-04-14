666fba194b641cab2d47854df61cc57b75804ac5
131MohitYadav <yadavjimohit54@gmail.com>
2026-04-14 22:39:31 +0530

    14 apr 25:

---
+++ All java  practise problems/src/StrOps.java
@@ -0,0 +1,26 @@
+public class StrOps
+public static void main (String[] args) {
+    String text = "Betty got a bit of bitter better butter";
+
+    // print all occruence ( index of 'b' and 't'
+
+    System.out.println("Indexes of 'b' and 't'");
+    for (int i = 0; i < text.length(); i++) {
+        char c =  Character.toLowerCase(text.charAt(i));
+        if ( c == 'b'){
+            System.out.println("'b' found at index " + i);
+        }
+        if ( c == 't'){
+            System.out.println("'t' found at index " + i);
+        }
+    }
+
+    // part (b): Print each word with its length
+    System.out.println("\nPart (b): EAch word with its length");
+    String[] words = text.split(" ");
+    for ( String word : words){
+        System.out.println(word + " : " + word.length());
+    }
+    );
+
+}
