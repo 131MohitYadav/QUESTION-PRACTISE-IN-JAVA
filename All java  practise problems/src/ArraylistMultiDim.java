11ee77f22aefa0da28c3cb59c5ece9c652b184f4
131MohitYadav <yadavjimohit54@gmail.com>
2026-04-14 22:48:21 +0530

    14 apr 26

--- All java  practise problems/src/ArraylistMultiDim.java
+++ All java  practise problems/src/ArraylistMultiDim.java
@@ -1,38 +1,38 @@
-// write a java programme to print the value of multidemisional arraylist
-// list 1 = 1 , 2, 3, 4, 5
-// list 2 = 2, 4, 6, 8 , 10
-// list 3 = 3 , 6 , 9 , 12 , 15
-
-import  java.util.ArrayList;
-public class ArraylistMultiDim {
-
-    public static void main(String args[]){
-
-        ArrayList<ArrayList<Integer>> Mainlist = new ArrayList<>();
-
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-        ArrayList<Integer> list3 = new ArrayList<>();
-        Arraylist<Integer> list4 = new Arraylist<>();
-
-
-
-        for(int i = 0 ; i<=5; i++){
-
-            list1.add(i * 1);
-            list2.add(i * 2);
-            list3.add(i * 3);
-            list4.add( i * 4);
-
-        }
-
-        Mainlist.add(list1);
-        Mainlist.add(list2);
-        Mainlist.add(list3);
-        Mainlist.add(list4);
-
-
-        System.out.println(Mainlist);
-
-    }
-}
+//// write a java programme to print the value of multidemisional arraylist
+//// list 1 = 1 , 2, 3, 4, 5
+//// list 2 = 2, 4, 6, 8 , 10
+//// list 3 = 3 , 6 , 9 , 12 , 15
+//
+//import  java.util.ArrayList;
+//public class ArraylistMultiDim {
+//
+//    public static void main(String args[]){
+//
+//        ArrayList<ArrayList<Integer>> Mainlist = new ArrayList<>();
+//
+//        ArrayList<Integer> list1 = new ArrayList<>();
+//        ArrayList<Integer> list2 = new ArrayList<>();
+//        ArrayList<Integer> list3 = new ArrayList<>();
+//        Arraylist<Integer> list4 = new Arraylist<>();
+//
+//
+//
+//        for(int i = 0 ; i<=5; i++){
+//
+//            list1.add(i * 1);
+//            list2.add(i * 2);
+//            list3.add(i * 3);
+//            list4.add( i * 4);
+//
+//        }
+//
+//        Mainlist.add(list1);
+//        Mainlist.add(list2);
+//        Mainlist.add(list3);
+//        Mainlist.add(list4);
+//
+//
+//        System.out.println(Mainlist);
+//
+//    }
+//}
--- All java  practise problems/src/StringValidation1.java
+++ All java  practise problems/src/StringValidation1.java
@@ -4,5 +4,16 @@ public class StringValidation1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if(input.length() < 6){
+            System.out.println("Error: The string must have at least 6 characters");
+        }
+        else if(!input.matches("[a-zA-Z0-9@]+]")) {
+            System.out.println("Error: The string must not contains special characters except @");
+        }
+        else{
+            System.out.println("Valid string: " + input);
+        }
     }
 }
