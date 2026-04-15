21150276e13e8219ae6d67774e760bdd71f472fe
131MohitYadav <yadavjimohit54@gmail.com>
2026-04-15 22:26:37 +0530

    15 apr 25

---
+++ JAVA_GFG_PRACTISE/src/BToD.java
@@ -0,0 +1,8 @@
+public class BToD {
+    // function to convet decimal to binary
+    static void decToBinary(int n){
+
+        // array to store binary number
+        int [] binaryNum = new int [1000];
+    }
+}
